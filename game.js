e7bdf60f45a270699decdd6a2ce61df813e65d38
Jared Stratton <Strattonshrugged@gmail.com>
2015-09-30 12:57:36 -0700

    initial commit, no changes

---
+++ game.js
@@ -0,0 +1,18 @@
+
+var question1 = prompt("Do I like chocolate?");
+console.log('Question 1 response: ' + question1);
+alert('Your answer to question 1 was ' + question1);
+
+
+var question2 = prompt('Do I like dogs?');
+console.log('Question 2 response: ' + question2);
+alert('Your answer to question 2 was ' + question2);
+
+
+var question3 = prompt('Do I like biking?');
+console.log('Question 3 response: ' + question3);
+alert('Your answer to question 3 was ' + question3);
+
+alert('So to summarize I like chocolate and dogs but hate biking');
+
+var answersRight = 0
