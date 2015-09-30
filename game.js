b7f2e044d0105cf520b2871c5f9812b96e56638b
Jared Stratton <Strattonshrugged@gmail.com>
2015-09-30 14:03:54 -0700

    Got question1 working and variable tracking

--- game.js
+++ game.js
@@ -1,9 +1,39 @@
+var answersRight = 0;
+var answer1;
+var answer2;
+var answer3;
 
-var question1 = prompt("Do I like chocolate?");
-console.log('Question 1 response: ' + question1);
-alert('Your answer to question 1 was ' + question1);
+var answer1 = prompt("Do I like chocolate?");
+alert("You said: " + answer1);
+console.log('answer1 exact input: ' + answer1);
+console.log('answer1 Upper Case: ' + answer1.toUpperCase());
 
 
+if (answer1.toUpperCase() === 'YES')	{
+	alert("Congratulations, that's correct!");
+	answersRight++;
+}	else	{
+	alert("Sorry, either you weren't clear or you have never seen me eat chocolate.");
+}
+
+
+document.write(answersRight);
+// so I can check to see if that variable is changing correctly
+
+
+/*	class demo
+if (question1.toUpperCase === 'YES')	{
+	alert('Lucky you');
+}	else	{
+	alert('Sucks to be you');
+}
+*/
+
+
+
+
+// Just cutting down to one question right now
+/*
 var question2 = prompt('Do I like dogs?');
 console.log('Question 2 response: ' + question2);
 alert('Your answer to question 2 was ' + question2);
@@ -14,6 +44,7 @@ console.log('Question 3 response: ' + question3);
 alert('Your answer to question 3 was ' + question3);
 
 alert('So to summarize I like chocolate and dogs but hate biking');
+*/
 
-var answersRight = 0
-// just changing this to see git change
+
+
