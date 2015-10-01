379c2eca2ff4b8094c9f7ad644f0e3b6b5359b12
jessethach <jessethach@gmail.com>
2015-10-01 13:14:22 -0700

    Move alerts to the DOM

--- game.js
+++ game.js
@@ -1,9 +1,8 @@
 
-
 var answersRight = 0;
-var answer1;
-var answer2;
-var answer3;
+var answer1 = document.getElementById('first');
+var answer2 = document.getElementById('second');
+var answer3 = document.getElementById('third');
 
 question1();
 question2();
@@ -16,10 +15,10 @@ function question1() {
 		console.log('answer1 Upper Case: ' + answer1.toUpperCase());
 
 	if ((answer1.toUpperCase() === 'YES') || (answer1.toUpperCase() === 'Y'))	{
-		alert("Congratulations, that's correct!");
+		first.innerHTML = "Congratulations, that's correct!";
 		answersRight++;
 	}	else	{
-		alert("Sorry, either you weren't clear or you have never seen me eat chocolate.");
+		first.innerHTML = "Sorry, either you weren't clear or you have never seen me eat chocolate.";
 	}
 }
 
@@ -30,10 +29,10 @@ function question2() {
 			console.log('answer2 Upper Case: ' + answer2.toUpperCase());
 
 	if ((answer2.toUpperCase() === 'YES') || (answer2.toUpperCase() === 'Y'))	{
-		alert("Right! Dogs are awesome! And cats are ... tolerable.");
+		second.innerHTML = "Right! Dogs are awesome! And cats are ... tolerable.";
 		answersRight++;
 	}	else	{
-		alert("Are you kidding? I love dogs!");
+		second.innerHTML = "Are you kidding? I love dogs!";
 	}	
 }
 
@@ -44,10 +43,10 @@ function question3() {
 			console.log('answer3 Upper Case: ' + answer3.toUpperCase());
 
 	if ((answer3.toUpperCase() === 'NO') || (answer3.toUpperCase() === 'N'))	{
-		alert("Correct! The last bike I rode was stationary and I still crashed it!");
+		third.innerHTML = "Correct! The last bike I rode was stationary and I still crashed it!";
 		answersRight++;
 	}	else	{
-		alert("Sorry, I hate biking.");
+		third.innerHTML = "Sorry, I hate biking.";
 	}	
 }
 
