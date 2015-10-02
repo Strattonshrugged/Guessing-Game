fab27ec634f1efc9243adf82f922f312d9b86757
Heidi Laursen <heidilaursen@yahoo.com>
2015-10-02 14:53:22 -0700

    Move question and answers to arrays

--- game.js
+++ game.js
@@ -5,14 +5,16 @@ var answer2 = document.getElementById('second');
 var answer3 = document.getElementById('third');
 var ending = document.getElementById('ending');
 
+var question = ["Do I like chocolate?", "Do I like dogs?", "Do I enjoy biking?"];
+var answer = ['YES', 'Y', 'NO', 'N'];
 
 function question1() {
-	var answer1 = prompt("Do I like chocolate?");
-		alert("You said: " + answer1);
+	var answer1 = prompt(question[0]);
+		//alert("You said: " + answer1);
 		console.log('answer1 exact input: ' + answer1);
 		console.log('answer1 Upper Case: ' + answer1.toUpperCase());
 
-	if ((answer1.toUpperCase() === 'YES') || (answer1.toUpperCase() === 'Y'))	{
+	if ((answer1.toUpperCase() === answer[0]) || (answer1.toUpperCase() === answer[1]))	{
 		first.innerHTML = "Congratulations, that's correct!";
 		answersRight++;
 	}	else	{
@@ -21,31 +23,31 @@ function question1() {
 }
 
 function question2() {
-	var answer2 = prompt("Do I like dogs?");
-		alert("You said: " + answer2);
+	var answer2 = prompt(question[1]);
+		//alert("You said: " + answer2);
 			console.log('answer2 exact input: ' + answer2);
 			console.log('answer2 Upper Case: ' + answer2.toUpperCase());
 
-	if ((answer2.toUpperCase() === 'YES') || (answer2.toUpperCase() === 'Y'))	{
+	if ((answer2.toUpperCase() === answer[0]) || (answer2.toUpperCase() === answer[1]))	{
 		second.innerHTML = "Right! Dogs are awesome! And cats are ... tolerable.";
 		answersRight++;
 	}	else	{
 		second.innerHTML = "Are you kidding? I love dogs!";
-	}	
+	}
 }
 
 function question3() {
-	var answer3 = prompt("Do I enjoy biking?");
-		alert("You said: " + answer3);
+	var answer3 = prompt(question[2]);
+		//alert("You said: " + answer3);
 			console.log('answer3 exact input: ' + answer3);
 			console.log('answer3 Upper Case: ' + answer3.toUpperCase());
 
-	if ((answer3.toUpperCase() === 'NO') || (answer3.toUpperCase() === 'N'))	{
+	if ((answer3.toUpperCase() === answer[2]) || (answer3.toUpperCase() === answer[3]))	{
 		third.innerHTML = "Correct! The last bike I rode was stationary and I still crashed it!";
 		answersRight++;
 	}	else	{
 		third.innerHTML = "Sorry, I hate biking.";
-	}	
+	}
 }
 
 function summary(answersRight) {
